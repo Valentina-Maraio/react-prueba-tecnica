53557f66f5014da8f39a64c7adcb268be7210c92
Valentina-Maraio <valentina.maraio89@gmail.com>
2022-08-25 15:18:17 +0200

    releaseYear and Poster Arts problem

--- src/pages/Results/Serie.js
+++ src/pages/Results/Serie.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import entries from '../../data/sample.json'
+import { Card, Grid, Row, Text } from "@nextui-org/react";
+
+//src={entry.images["Poster Art"].url}
+// && 'releaseYear' >= '2010'
 
 const Serie = () => {
+  const size = 20;
+
   return (
-    <div>Serie</div>
+    <Grid.Container gap={2} justify="flex-start">
+      {entries?.entries.slice(0,size).map(entry =>
+        <Grid xs={6} sm={3}>
+          <Card isPressable>
+            <Card.Body css={{ p: 0 }}>
+              <Card.Image
+                src={entry.images["Poster Art"].url}
+                objectFit="cover"
+                width="100%"
+                height={140}
+                alt={entry.title}
+              />
+            </Card.Body>
+            <Card.Footer css={{ justifyItems: "flex-start" }}>
+              <Row wrap="wrap" justify="space-between" align="center">
+                <Text b>{entry.title}</Text>
+                <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
+                  {entry.releaseYear}
+                </Text>
+              </Row>
+            </Card.Footer>
+          </Card>
+        </Grid>
+      )}
+    </Grid.Container>
   )
 }
 
